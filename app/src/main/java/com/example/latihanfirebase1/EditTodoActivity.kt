ec1bd110a48ceb48619b6f551efb8c738474be4e
FerdiansyahDwi <ferdiansyahdwi00@gmail.com>
2025-10-20 19:11:03 +0700

    membuat halaman update delete todo activity baru

--- app/src/main/java/com/example/latihanfirebase1/EditTodoActivity.kt
+++ app/src/main/java/com/example/latihanfirebase1/EditTodoActivity.kt
@@ -2,12 +2,14 @@ package com.example.latihanfirebase1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.latihanfirebase1.databinding.ActivityEditTodoBinding
+import com.example.latihanfirebase1.entity.Todo
 import com.example.latihanfirebase1.usecase.TodoUseCase
 import kotlinx.coroutines.launch
 
@@ -31,15 +33,15 @@ class EditTodoActivity : AppCompatActivity() {
 
         todoItemId = intent.getStringExtra("todo_item_id").toString()
         todoUseCase = TodoUseCase()
+        registerEvents()
     }
 
     fun loadTodo() {
         lifecycleScope.launch {
             val todo = todoUseCase.getTodo(todoItemId)
             if (todo == null) {
-                val intent = Intent(this@EditTodoActivity, TodoActivity::class.java)
-                startActivity(intent)
-                finish()
+                displayMessage("Data yang akan di edit tidak tersedia")
+                back()
             }
 
             binding.title.setText(todo?.title)
@@ -47,8 +49,40 @@ class EditTodoActivity : AppCompatActivity() {
         }
     }
 
+    fun back() {
+        val intent = Intent(this@EditTodoActivity, TodoActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
     override fun onStart() {
         super.onStart()
         loadTodo()
+    }
+
+    fun registerEvents() {
+        binding.tombolEdit.setOnClickListener {
+            lifecycleScope.launch {
+                val title = binding.title.text.toString()
+                val description = binding.description.text.toString()
+                val payload = Todo(
+                    id = todoItemId,
+                    title = title,
+                    description = description,
+                )
+
+                try {
+                    todoUseCase.updateTodo(payload)
+                    displayMessage("Berhasil memperbarui data")
+                    back()
+                } catch (exc: Exception) {
+                    displayMessage("Gagal memperbarui data : ${exc.message}")
+                }
+            }
+        }
+    }
+
+    fun displayMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
